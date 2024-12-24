a6c8925d736fc23814191e75f7059770dfd84235
LAPTOP-BIVMS7AP\putug <putugenin@gmail.com>
2024-12-24 23:05:39 +0800

    end tutorial 4

--- app/scenes/game_scene.rb
+++ app/scenes/game_scene.rb
@@ -5,12 +5,13 @@ module Scenes
     end
 
     def self.render(state, sprites, labels)
-      sprites << [state.map.tiles, state.player]
+      sprites << [state.map.tiles, state.player, state.enemies]
     end
 
     def self.reset(state)
       ::Controllers::MapController.load_map(state)
       state.player = ::Entities::Player.spawn(2, 2)
     end
+
   end
 end
